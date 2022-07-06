f585357f732b0dc7e97e6de9aab77f34758dc011
DDing77 <smg76262585@gmail.com>
2022-07-07 00:52:32 +0900

    02
    1. PersistenctContext
    2. flush()
    2. detach()

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # JPAstudy
+
+#### Java version - 17
+#### dependency - maven
+#### springboot - 2.7.1
+#### JPA implement - hibernate 5.6.9.FINAL
+#### DB - h2database - 1.4.199
--- ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -18,15 +18,11 @@ public class JpaMain {
         tx.begin();
 
         try{
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
 
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+            Member findMember = em.find(Member.class, 150L);
+            findMember.setName("ZZZZZ");
+
+            System.out.println("=================");
 
             tx.commit();
         } catch (Exception e) {
